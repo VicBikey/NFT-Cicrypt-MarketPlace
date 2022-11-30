fecd65f2d1e37f962c83ba879fa0ebcba92dd0a9
VicBikey <chibykev1@gmail.com>
2022-11-30 13:32:50 +0100

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,15 @@
 import React from "react";
 
+//INERNAL IMPORT
+import Style from "../styles/index.module.css";
+import { HeroSection } from "../components/componentsindex";
+
 const index = () => {
-  return <div>index</div>;
+  return (
+    <div className={Style.homePage}>
+      <HeroSection />
+    </div>
+  )
 };
 
 export default index;
