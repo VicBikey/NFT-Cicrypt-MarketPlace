4b46f478954ed364bd3cd38fa03b7073dad6a79b
VicBikey <chibykev1@gmail.com>
2022-11-30 22:44:34 +0100

    first commit

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,14 @@ import React from "react";
 
 //INERNAL IMPORT
 import Style from "../styles/index.module.css";
-import { HeroSection, Service } from "../components/componentsindex";
+import { HeroSection, Service, BigNFTSlider } from "../components/componentsindex";
 
 const Home = () => {
   return (
     <div className={Style.homePage}>
       <HeroSection />
       <Service />
+      <BigNFTSlider />
     </div>
   )
 };
